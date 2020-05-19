7c0d907a51cd23e35701b49fcb6b650f2afe92ce
DenisViskov <denisviskov16@gmail.com>
2020-05-19 21:01:14 +0300

    update ProxyChanger

--- src/main/java/sqlruparse/ProxyChanger.java
+++ src/main/java/sqlruparse/ProxyChanger.java
@@ -10,10 +10,29 @@ package sqlruparse;
 public class ProxyChanger {
 
     /**
+     * Status
+     */
+    private static boolean status = false;
+
+    /**
      * Method turns on exit to network through Proxy
      */
     public static void useThroughProxy() {
         System.setProperty("https.proxyHost", "192.168.111.102");
         System.setProperty("https.proxyPort", "3128");
+        status = true;
+    }
+
+    /**
+     * Method turns off exit to network through Proxy
+     */
+    public static void useThroughDefaultNetwork() {
+        System.clearProperty("https.proxyHost");
+        System.clearProperty("https.proxyPort");
+        status = false;
+    }
+
+    public static boolean isStatus() {
+        return status;
     }
 }
