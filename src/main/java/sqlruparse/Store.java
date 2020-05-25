88fc442f5692df3b19a1f926be948fb82254fb40
DenisViskov <denisviskov16@gmail.com>
2020-05-25 10:30:33 +0300

    3. Архитектура проекта - Аргегатора Java Вакансий [#260359]

---
+++ src/main/java/sqlruparse/Store.java
@@ -0,0 +1,26 @@
+package sqlruparse;
+
+import java.util.List;
+
+/**
+ * Interface for store data
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 25.05.2020
+ */
+public interface Store {
+    /**
+     * Method should save data to our storage
+     *
+     * @param post - post
+     */
+    void save(Post post);
+
+    /**
+     * Method should return all element from storage
+     *
+     * @return - list of posts
+     */
+    List<Post> getAll();
+}
