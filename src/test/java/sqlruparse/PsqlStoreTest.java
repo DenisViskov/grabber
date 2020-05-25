a0e80c5a48aea0a04e8d33fc65b6869a64eccbd9
DenisViskov <denisviskov16@gmail.com>
2020-05-25 13:48:13 +0300

    updated

--- src/test/java/sqlruparse/PsqlStoreTest.java
+++ src/test/java/sqlruparse/PsqlStoreTest.java
@@ -1,6 +1,8 @@
 package sqlruparse;
 
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -11,6 +13,7 @@ import org.mockito.stubbing.Answer;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
@@ -19,16 +22,21 @@ import static org.mockito.Mockito.*;
 public class PsqlStoreTest {
 
     @Mock
-    Connection connection;
+    private Connection connection;
     @Mock
-    PreparedStatement statement;
+    private PreparedStatement statement;
     @Mock
-    Statement defaultStatement;
+    private Statement defaultStatement;
+    @Mock
+    private ResultSet resultSet;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void saveTest() throws SQLException {
-        Connection connection = mock(Connection.class);
-        PreparedStatement statement = mock(PreparedStatement.class);
         when(connection.prepareStatement(any())).thenReturn(statement);
         when(statement.executeUpdate()).thenReturn(1);
         PsqlStore store = new PsqlStore(connection);
@@ -40,11 +48,8 @@ public class PsqlStoreTest {
     public void getAllTest() throws SQLException {
         List<Post> expected = List.of(new Post("sgsdfg", "sgsdfg", "sgsdfg", LocalDateTime.now()),
                 new Post("sgsdfg", "sgsdfg", "sgsdfg", LocalDateTime.now()));
-        Connection connection = mock(Connection.class);
-        Statement statement = mock(Statement.class);
-        ResultSet resultSet = mock(ResultSet.class);
-        when(connection.createStatement()).thenReturn(statement);
-        when(statement.executeQuery(any())).thenReturn(resultSet);
+        when(connection.createStatement()).thenReturn(defaultStatement);
+        when(defaultStatement.executeQuery(any())).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(true).thenReturn(true).thenReturn(false);
         when(resultSet.getString(any())).thenReturn("sgsdfg");
         when(resultSet.getTimestamp(any())).thenReturn(Timestamp.valueOf(LocalDateTime.now()));
@@ -56,9 +61,6 @@ public class PsqlStoreTest {
     @Test
     public void findByIdTest() throws SQLException {
         Post expected = new Post("sgsdfg", "sgsdfg", "sgsdfg", LocalDateTime.now());
-        Connection connection = mock(Connection.class);
-        PreparedStatement statement = mock(PreparedStatement.class);
-        ResultSet resultSet = mock(ResultSet.class);
         when(connection.prepareStatement(any())).thenReturn(statement);
         when(statement.executeQuery()).thenReturn(resultSet);
         when(resultSet.next()).thenReturn(true).thenReturn(false);
@@ -71,8 +73,7 @@ public class PsqlStoreTest {
 
     @Test
     public void whenSaveCatchSQLExceptionTest() throws SQLException {
-        Connection connection = mock(Connection.class);
-        doThrow(SQLException.class).when(connection).prepareStatement(any());
+        when(connection.prepareStatement(anyString())).thenThrow(SQLException.class);
         PsqlStore store = new PsqlStore(connection);
         boolean out = store.save(new Post("sgsdfg", "name", "sadfgsd", LocalDateTime.now()));
         assertThat(out, is(false));
@@ -80,7 +81,6 @@ public class PsqlStoreTest {
 
     @Test
     public void whengetAllCatchSQLExceptionTest() throws SQLException {
-        Connection connection = mock(Connection.class);
         when(connection.createStatement()).thenThrow(SQLException.class);
         PsqlStore store = new PsqlStore(connection);
         List<Post> out = store.getAll();
@@ -89,15 +89,9 @@ public class PsqlStoreTest {
 
     @Test
     public void whenfindByIdCatchSQLExceptionTest() throws SQLException {
-        Connection connection = mock(Connection.class);
-        when(connection.prepareStatement(any())).thenThrow(SQLException.class);
+        doThrow(SQLException.class).when(connection).prepareStatement(anyString());
         PsqlStore store = new PsqlStore(connection);
-        Post out = store.findById("");
+        Post out = store.findById("1");
         assertThat(out, is(isNull()));
-    }
-
-    @After
-    public void downMock() {
-        Mockito.reset();
     }
 }
